c815f37a9e3b60582880bd532b4b51555a509c78
Miguel <miguel.ramirez@ohmyfi.com>
2022-01-22 12:12:35 -0500

    diseño del login

---
+++ login.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Login</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+  <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-4">
+                
+            </div>
+
+            <div class="col-md-4">
+
+            <form action="login.php" method="post">
+                Usuario:<input class="form-control" type="text" name="usuario" id="">
+                <br/>
+                Contraseña: <input class="form-control" type="text" name="clave" id="">
+                <br/>
+                
+                <button class="btn btn-success" type="submit">Entrar al portafolio</button>
+            </form>
+                
+            </div>
+
+            <div class="col-md-4">
+                
+            </div>
+
+        </div>
+
+
+  </div>
+
+  </body>
+</html>
